452a911a6cc944a2d20f4eeedad7e0feea08ad5b
Jonathan Lehr <jonathan@aboutobjects.com>
2020-07-24 10:10:13 -0400

    Friday early afternoon WIP

--- ReadingList/ReadingList/RELReadingListController.m
+++ ReadingList/ReadingList/RELReadingListController.m
@@ -2,8 +2,21 @@
 // See LICENSE.txt for this project's licensing information.
 
 #import "RELReadingListController.h"
+#import <ReadingListModel/ReadingListModel.h>
+
+@interface RELReadingListController ()
+@property (strong, nonatomic) IBOutlet RLMStoreController *storeController;
+@property (strong, nonatomic) RLMReadingList *readingList;
+@end
 
 @implementation RELReadingListController
+
+- (RLMReadingList *)readingList {
+    if (_readingList == nil) {
+        _readingList = self.storeController.fetchedReadingList;
+    }
+    return _readingList;
+}
 
 // MARK: - Unwind segues
 
@@ -13,6 +26,19 @@
 
 - (IBAction)cancel:(UIStoryboardSegue *)unwindSegue {
     
+}
+
+// MARK: - UITableViewDataSource methods
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.readingList.books.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Book Cell"];
+    RLMBook *book = [self.readingList bookAtIndexPath:indexPath];
+    cell.textLabel.text = book.title;
+    return cell;
 }
 
 @end
